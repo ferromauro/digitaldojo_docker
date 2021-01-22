44d2b1f222199d28997ba5ec666e97167f274638
Ubuntu <ubuntu@ip-172-31-19-158.us-east-2.compute.internal>
2021-01-22 21:18:16 +0000

    Some fixes

--- Modulo_6/setup.sh
+++ Modulo_6/setup.sh
@@ -5,23 +5,20 @@
 echo "Creazione del file .pem"
 aws ec2 create-key-pair --key-name MyKeyPair --query 'KeyMaterial' --output text > MyKeyPair.pem
 
-# Aggiungo delle restizioni sul file 
-chmod 400 MyKeyPair.pem
-
 # Creazione di un security-group
 echo "Creazione del gruppo di sicurezza"
 aws ec2 create-security-group --group-name my-sg --description "My security group"
 
-# Salvataggio IP AWS in una variabile
-echo "Recupero AWS IP"
-AWS_IP=$(curl https://checkip.amazonaws.com) 
-
 # Creazione delle regole per abilitare la connessione ssh
+
+###################### !!! NOTA BENE !!! ############################
+# Viene aperta una porta 22 con accesso senza restrizioni 0.0.0.0/0 #
+#####################################################################
 echo "Creazione security-group"
-aws ec2 authorize-security-group-ingress --group-name my-sg --protocol tcp --port 22 --cidr $AWS_IP/24
+aws ec2 authorize-security-group-ingress --group-name my-sg --protocol tcp --port 22 --cidr 0.0.0.0/0
 
 # Creazione istanze EC2
 echo "Creazione istance EC2:"
-aws ec2 run-instances --image-id ami-0a91cd140a1fc148a --count 2 --instance-type t2.micro --key-name MyKeyPair --security-groups my-sg
+aws ec2 run-instances --image-id ami-0a91cd140a1fc148a --count 3 --instance-type t2.micro --key-name MyKeyPair --security-groups my-sg --user-data file://bootstrap.txt
 
 echo "SETUP COMPLETATO!"
